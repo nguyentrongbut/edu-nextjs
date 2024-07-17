42ee77c4a501bd7d89ced40e698308e029d1aabf
Bút <Cloly031128@gmail.com>
2024-07-18 00:57:04 +0700

    deploy(test): test deploy

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -16,7 +16,6 @@ export default async function Home() {
                 <CourseItem></CourseItem>
                 <CourseItem></CourseItem>
                 <CourseItem></CourseItem>
-                <CourseItem></CourseItem>
             </CourseGrid>
         </div>
     );
