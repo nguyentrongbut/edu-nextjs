3d740908d1b639ad36b3fb58367319411111d049
Bút <Cloly031128@gmail.com>
2024-07-18 00:39:05 +0700

    deploy(test): test deploy

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -16,6 +16,7 @@ export default async function Home() {
                 <CourseItem></CourseItem>
                 <CourseItem></CourseItem>
                 <CourseItem></CourseItem>
+                <CourseItem></CourseItem>
             </CourseGrid>
         </div>
     );
